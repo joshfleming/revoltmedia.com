25c1162e16d49058ec9f18c0d87ec9e8841bc85e
JOsh F <go@revoltmedia.com>
2019-03-01 20:51:46 -0800

    remove contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,6 @@ module.exports = {
     title: `Revolt Media`
   },
   plugins: [
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `y3vpeex0g3xs`,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
     `gatsby-transformer-remark`
   ],
 }
