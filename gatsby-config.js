24882322dc3c6cf8b01ae10c218546d06a63f5e1
JOsh F <go@revoltmedia.com>
2019-03-04 18:31:32 -0800

    icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,39 +15,40 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: "./src/favicon.png",
-
-        // WebApp Manifest Configuration
-        appName: null, // Inferred with your package.json
-        appDescription: null,
-        developerName: null,
-        developerURL: null,
-        dir: 'auto',
-        lang: 'en-US',
-        background: '#fff',
-        theme_color: '#fff',
-        display: 'standalone',
-        orientation: 'any',
-        start_url: '/?homescreen=1',
-        version: '1.0',
-
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: true,
-          favicons: true,
-          firefox: true,
-          opengraph: true,
-          twitter: true,
-          yandex: true,
-          windows: true
-        }
-      }
-    },
+    `gatsby-plugin-react-helmet`,
+    // {
+    //   resolve: `gatsby-plugin-favicon`,
+    //   options: {
+    //     logo: "./src/favicon.png",
+    //
+    //     // WebApp Manifest Configuration
+    //     appName: null, // Inferred with your package.json
+    //     appDescription: null,
+    //     developerName: null,
+    //     developerURL: null,
+    //     dir: 'auto',
+    //     lang: 'en-US',
+    //     background: '#fff',
+    //     theme_color: '#fff',
+    //     display: 'standalone',
+    //     orientation: 'any',
+    //     start_url: '/?homescreen=1',
+    //     version: '1.0',
+    //
+    //     icons: {
+    //       android: true,
+    //       appleIcon: true,
+    //       appleStartup: true,
+    //       coast: true,
+    //       favicons: true,
+    //       firefox: true,
+    //       opengraph: true,
+    //       twitter: true,
+    //       yandex: true,
+    //       windows: true
+    //     }
+    //   }
+    // },
     {
       resolve: `gatsby-plugin-sass`,
       options: {
